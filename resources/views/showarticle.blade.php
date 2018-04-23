04e826a0e66442aa43108646556938c393364e45
stevannohero <13515082@std.stei.itb.ac.id>
2018-04-24 01:06:04 +0700

    edit show article

--- resources/views/showarticle.blade.php
+++ resources/views/showarticle.blade.php
@@ -3,62 +3,16 @@
 @section('title', 'Posts')
 
 @section('content')
-<div class="container-fluid">
+<div class="body-qna">
   <div class="row">
-    <nav class="col-3 sidebar">
-      <div class="sidebar-sticky">
-        <ul class="nav flex-column">
-        <li class="nav-item" id="nav-two">
-            <a class="nav-link" href="/members">
-              <span id="text-nav-two">
-                <span data-feather="users"></span>
-                <i class="sideMenu">Members</i>
-              </span>    
-            </a>
-          </li>
-          <li class="nav-item" id="nav-one">
-            <a class="nav-link" href="/forum">
-              <span id="text-nav-one">  
-                <span data-feather="home"></span>
-                <i class="sideMenu">Forum</i><span class="sr-only">(current)</span>
-              </span>    
-            </a>
-          </li>
-          <li class="nav-item" id="nav-three">
-            <a class="nav-link" href="/posts">
-              <span id="text-nav-three">
-                <span data-feather="file"></span>
-                <i class="sideMenu">Posts</i>
-              </span>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  <!-- Icons -->
-  <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
-  <script>
-    feather.replace();
-  </script>
-    
-  <main role="main" class="col-7">
-  <span>
-        <h1 class="post-title">{{$post->title}}</h1>
-        <h5 class="post-title">&nbsp;
-            @if ($post->draft == '1')
-                <span style="color:red;">Draft</span>
-            @else
-                <span style="color:green;">Published</span>
-            @endif
-        </h5>
-    </span>  
-    
-    <div class="body-article">
-        {!!$post->body!!}
-    </div>
-
-    <div class="footer-article">
-        <hr>
+    <div class="col-2"></div>
+      <div class="well question-container col-8" style="margin-top: 50px;">
+        <h1>{{$post->title}}</h1>
+        @if ($post->draft == '1')
+          <span style="color:red;">Draft</span>
+        @else
+          <span style="color:green;">Published</span>
+        @endif
         <h5>
             @if ($post->public == '1')
                 Public 
@@ -67,6 +21,11 @@
             @endif  
             Post
         </h5>
+        <hr>
+        <div >{!!$post->body!!}</div>
+        <!-- FOOTER -->
+        <div class="footer-article">
+        <hr>
         <small>Written on {{$post->created_at}}</small><br>
         <small>Last Editted on {{$post->updated_at}}</small><br>
         <small>by {{$post->user->name}}</small>
@@ -79,10 +38,10 @@
                 {{Form::submit('Delete', ['class' => 'btn btn-danger', 'onclick' => "return confirm('Are you sure you want to delete?')"])}}
             {!!Form::close() !!}
         @endif
-        <br><br><br>
         <a href="/posts" class="btn btn-info pull-down">&#8592; Back</a>
-        
-    </div>
-  </main>
-
+        </div>
+      </div> <!-- END OF WELL -->
+    <div class="col-2"></div>
+  </div>
+</div>
 @endsection
